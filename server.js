d5febe080a5c7586544cf7134d3783d7ff5a5b4c
ianalexanderz <ianstewart745@gmail.com>
2021-02-24 00:55:46 -0800

    more to do

--- server.js
+++ server.js
@@ -4,14 +4,19 @@ const rowdy = require('rowdy-logger')
 
 const rowdyRes = rowdy.begin(app)
 
-
-
+// Middleware
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(ejsLayouts)
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
 
 app.get('/', (req, res) => {
   res.send('hello from root!')
 })
 
+// Controllers
 app.use('/countries', require('./controllers/countriesController'))
 app.use('/continents', require('./controllers/continentsController'))
 
